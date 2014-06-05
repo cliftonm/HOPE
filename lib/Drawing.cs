2857569d7d936b3c1e0d203fdd783e73bd705209
Marc Clifton <marc.clifton@gmail.com>
2014-06-05 19:44:29 -0400

    Trying a different thread manager.

---
+++ lib/Drawing.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clifton.Drawing
+{
+	public static class DrawingExtensionMethods
+	{
+		public static Size Delta(this Point p1, Point p2)
+		{
+			return new Size(p2.X - p1.X, p2.Y - p1.Y);
+		}
+	}
+}
