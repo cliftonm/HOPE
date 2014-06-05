06ceff4574d41ed0024195a3b68d286fc9931255
Marc Clifton <marc.clifton@gmail.com>
2014-06-05 19:44:42 -0400

    Trying a different thread manager.

--- Receptors/ThumbnailCreatorReceptor/ReceptorDefinition.cs
+++ Receptors/ThumbnailCreatorReceptor/ReceptorDefinition.cs
@@ -4,9 +4,14 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-// using System.Threading.Tasks;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+using Clifton.ExtensionMethods;
 using Clifton.Tools.Strings.Extensions;
+using Clifton.Threading;
+
 using Clifton.Receptor.Interfaces;
 using Clifton.SemanticTypeSystem.Interfaces;
 
@@ -43,23 +48,53 @@ namespace ThumbnailCreatorReceptor
 		{
 		}
 
-		// was public void async...
-		public void ProcessCarrier(ICarrier carrier)
+		protected void ProcessImage(object state)
+		{
+			string fn = (string)state;
+
+			Bitmap bitmap = new Bitmap(fn);
+			// Reduce the size of the image.  If we don't do this, scrolling and rendering of scaled images is horrifically slow.
+			Image image = new Bitmap(bitmap, 256, 256 * bitmap.Height / bitmap.Width);
+			image.Tag = fn;
+			bitmap.Dispose();
+
+			((Control)Application.OpenForms[0]).BeginInvoke(() => OutputImage(fn, image));
+			Thread.Sleep(100);
+		}
+
+		public async void ProcessCarrier(ICarrier carrier)
 		{
 			if (carrier.Signal.Filename != null)
 			{
 				string fn = carrier.Signal.Filename;
-
 				// Only process if the file exists.
 				if (File.Exists(fn))
 				{
+//					ManagedThreadPool.QueueUserWorkItem(new WaitCallback(ProcessImage), fn);
+
+					Image ret = await Task.Run<Image>(() =>
+					{
+						Bitmap bitmap = new Bitmap(fn);
+						// Reduce the size of the image.  If we don't do this, scrolling and rendering of scaled images is horrifically slow.
+						Image image = new Bitmap(bitmap, 256, 256 * bitmap.Height / bitmap.Width);
+						image.Tag = fn;
+						bitmap.Dispose();
+
+						return image;
+					});
+
+					OutputImage(fn, ret);
+
 					// This is fast enough we don't need to run this as a separate thread unless these files are perhaps coming from a slow network.
+					// - no, we can leverage multiple cores when we're processing a whole swarm of images 
+					/*
 					Bitmap bitmap = new Bitmap(fn);
 					// Reduce the size of the image.  If we don't do this, scrolling and rendering of scaled images is horrifically slow.
 					Image image = new Bitmap(bitmap, 256, 256 * bitmap.Height / bitmap.Width);
 					image.Tag = fn;
 					bitmap.Dispose();
 					OutputImage(fn, image);
+					*/
 				}
 				else
 				{
@@ -70,20 +105,6 @@ namespace ThumbnailCreatorReceptor
 			{
 				NoFilenameProvided();
 			}
-
-/*
-			Image ret = await Task.Run<Image>(() =>
-				{
-					Bitmap bitmap = new Bitmap(fn);
-					// Reduce the size of the image.  If we don't do this, scrolling and rendering of scaled images is horrifically slow.
-					Image image = new Bitmap(bitmap, 256, 256 * bitmap.Height / bitmap.Width);
-					image.Tag = fn;
-					bitmap.Dispose();
-
-					return image;
-				});
-			OutputImage(fn, ret);
-*/
 		}
 
 		protected void FileMissing(string fn)
