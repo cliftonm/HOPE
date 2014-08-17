eda0df16737824ff1fa72de318d49a8a318d6a4f
Marc Clifton <marc.clifton@gmail.com>
2014-08-16 22:00:04 -0400

    Added number to text conversion.  Eventually we should add this to lib instead.

--- Receptors/Fun Stuff/DayGreeterReceptor/DayGreeter.cs
+++ Receptors/Fun Stuff/DayGreeterReceptor/DayGreeter.cs
@@ -76,7 +76,7 @@ namespace DayGreeterReceptor
 						break;
 
 					case "month":
-						tokenValue = now.ToString("MMM", CultureInfo.InvariantCulture);
+						tokenValue = now.ToString("MMMM", CultureInfo.InvariantCulture);
 						break;
 
 					case "dateofmonth":
@@ -85,11 +85,11 @@ namespace DayGreeterReceptor
 
 					case "hour12":
 						int h = now.Hour % 12;
-						tokenValue = h.ToString();
+						tokenValue = NumWordsWrapper(h);
 						break;
 
 					case "minute":
-						tokenValue = now.Minute.ToString();
+						tokenValue = NumWordsWrapper(now.Minute);
 						break;
 
 					case "ampm":
@@ -106,5 +106,114 @@ namespace DayGreeterReceptor
 
 			return sb.ToString();
 		}
+
+		// http://www.codeproject.com/Questions/455350/how-to-convert-numbers-into-texual-format-using-cs
+		protected String NumWordsWrapper(double n)
+		{
+			string words = "";
+			double intPart;
+			double decPart = 0;
+			if (n == 0)
+				return "zero";
+			try
+			{
+				string[] splitter = n.ToString().Split('.');
+				intPart = double.Parse(splitter[0]);
+				decPart = double.Parse(splitter[1]);
+			}
+			catch
+			{
+				intPart = n;
+			}
+
+			words = NumWords(intPart);
+
+			if (decPart > 0)
+			{
+				if (words != "")
+					words += " and ";
+				int counter = decPart.ToString().Length;
+				switch (counter)
+				{
+					case 1: words += NumWords(decPart) + " tenths"; break;
+					case 2: words += NumWords(decPart) + " hundredths"; break;
+					case 3: words += NumWords(decPart) + " thousandths"; break;
+					case 4: words += NumWords(decPart) + " ten-thousandths"; break;
+					case 5: words += NumWords(decPart) + " hundred-thousandths"; break;
+					case 6: words += NumWords(decPart) + " millionths"; break;
+					case 7: words += NumWords(decPart) + " ten-millionths"; break;
+				}
+			}
+			return words;
+		}
+
+		protected String NumWords(double n) //converts double to words
+		{
+			string[] numbersArr = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+			string[] tensArr = new string[] { "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+			string[] suffixesArr = new string[] { "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septdecillion", "Octodecillion", "Novemdecillion", "Vigintillion" };
+			string words = "";
+
+			bool tens = false;
+
+			if (n < 0)
+			{
+				words += "negative ";
+				n *= -1;
+			}
+
+			int power = (suffixesArr.Length + 1) * 3;
+
+			while (power > 3)
+			{
+				double pow = Math.Pow(10, power);
+				if (n > pow)
+				{
+					if (n % Math.Pow(10, power) > 0)
+					{
+						words += NumWords(Math.Floor(n / pow)) + " " + suffixesArr[(power / 3) - 1] + ", ";
+					}
+					else if (n % pow > 0)
+					{
+						words += NumWords(Math.Floor(n / pow)) + " " + suffixesArr[(power / 3) - 1];
+					}
+					n %= pow;
+				}
+				power -= 3;
+			}
+			if (n >= 1000)
+			{
+				if (n % 1000 > 0) words += NumWords(Math.Floor(n / 1000)) + " thousand, ";
+				else words += NumWords(Math.Floor(n / 1000)) + " thousand";
+				n %= 1000;
+			}
+			if (0 <= n && n <= 999)
+			{
+				if ((int)n / 100 > 0)
+				{
+					words += NumWords(Math.Floor(n / 100)) + " hundred";
+					n %= 100;
+				}
+				if ((int)n / 10 > 1)
+				{
+					if (words != "")
+						words += " ";
+					words += tensArr[(int)n / 10 - 2];
+					tens = true;
+					n %= 10;
+				}
+
+				if (n < 20)
+				{
+					if (words != "" && tens == false)
+						words += " ";
+					words += (tens ? "-" + numbersArr[(int)n - 1] : numbersArr[(int)n - 1]);
+					n -= Math.Floor(n);
+				}
+			}
+
+			return words;
+		}
+
     }
 }
