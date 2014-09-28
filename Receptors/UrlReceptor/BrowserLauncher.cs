13dd45d44908db3477fbd6a5857102c897d293da
Marc Clifton <marc.clifton@gmail.com>
2014-09-27 20:51:17 -0400

    New source file name.

---
+++ Receptors/UrlReceptor/BrowserLauncher.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Clifton.Receptor.Interfaces;
+using Clifton.SemanticTypeSystem.Interfaces;
+using Clifton.Tools.Strings.Extensions;
+
+namespace UrlReceptor
+{
+	public class ReceptorDefinition : BaseReceptor
+	{
+		public override string Name { get { return "Web Page Launcher"; } }
+		
+		public ReceptorDefinition(IReceptorSystem rsys) : base(rsys)
+		{
+			AddReceiveProtocol("Url");
+		}
+
+		public override void ProcessCarrier(ICarrier carrier)
+		{
+			string url = carrier.Signal.Value;
+			try
+			{
+				Process.Start(url);
+			}
+			catch
+			{
+				// Eat exceptions.
+			}
+		}
+	}
+}
