9007542a89681b5510e957ce2f989c6d53f14f88
Marc Clifton <marc.clifton@gmail.com>
2014-10-19 18:51:44 -0400

    Added binding helper for mapping control properties to receptor properties.

---
+++ Clifton.Receptor/Binding.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clifton.Receptor
+{
+	public class Binding : ISupportInitialize
+	{
+		public string PropertyName { get; set; }
+		public Control Control { get; set; }
+		public object Source { get; set; }
+		public string ControlPropertyName { get; set; }
+
+		public void BeginInit()
+		{
+		}
+
+		public void EndInit()
+		{
+			Control.DataBindings.Add(new System.Windows.Forms.Binding(ControlPropertyName, Source, PropertyName));
+		}
+	}
+}
