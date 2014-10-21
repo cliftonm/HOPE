3eb9984e4df59622b09f6559ae450d8e1cee35b7
Marc Clifton <marc.clifton@gmail.com>
2014-10-21 08:42:19 -0400

    Added a couple missing C# files.

---
+++ Clifton.Receptor/BindingSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clifton.Receptor
+{
+	public class BindingSource : ISupportInitialize
+	{
+		public string PropertyName { get; set; }
+		public Control Control { get; set; }
+		public object Source { get; set; }
+		public string ControlPropertyName { get; set; }
+
+		public void BeginInit()
+		{
+		}
+
+		public void EndInit()
+		{
+			System.Windows.Forms.BindingSource bs = new System.Windows.Forms.BindingSource();
+			PropertyInfo pi = Source.GetType().GetProperty(PropertyName);
+			object src = pi.GetValue(Source);
+			bs.DataSource = src;
+			pi = Control.GetType().GetProperty("DataSource");
+			pi.SetValue(Control, bs);
+		}
+	}
+}
