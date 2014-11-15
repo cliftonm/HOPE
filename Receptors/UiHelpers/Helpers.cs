8c00742867695a7b31673a8f63add301cd68943f
Marc Clifton <marc.clifton@gmail.com>
2014-11-14 21:50:53 -0500

    UI helpers and signal creator test applet.

---
+++ Receptors/UiHelpers/Helpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Clifton.Receptor.Interfaces;
+
+namespace ReceptorUiHelpers
+{
+	public interface ISupportModelessConfiguration
+	{
+		bool Modeless { get; }
+	}
+
+    public static class Helper
+    {
+		public static List<string> PopulateProtocolComboBox(ComboBox cbProtocols, IReceptorSystem rsys, string protocolName)
+		{
+			List<string> types = rsys.SemanticTypeSystem.SemanticTypes.Keys.ToList();
+			types.Sort();
+			cbProtocols.DataSource = types;
+
+			if (!String.IsNullOrEmpty(protocolName))
+			{
+				cbProtocols.SelectedItem = protocolName;
+			}
+
+			return types;
+		}
+	}
+}
