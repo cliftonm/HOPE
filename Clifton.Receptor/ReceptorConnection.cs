b882fa79517540338ada75ec6622799d3ecd51ae
Marc Clifton <marc.clifton@gmail.com>
2014-12-06 20:53:33 -0500

    Added a ReceptorConnection class that can hold optional behavior flags for how two receptors connect.

---
+++ Clifton.Receptor/ReceptorConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Clifton.Receptor.Interfaces;
+
+namespace Clifton.Receptor
+{
+	public class ReceptorConnection : IReceptorConnection
+	{
+		public IReceptor Receptor { get; set; }
+		public bool RootOnly { get; set; }
+
+		public ReceptorConnection(IReceptor receptor)
+		{
+			Receptor = receptor;
+		}
+
+		public ReceptorConnection(IReceptor receptor, bool rootOnly)
+		{
+			Receptor = receptor;
+			RootOnly = rootOnly;
+		}
+	}
+}
