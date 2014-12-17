6f5aff3f858489f52e166486985d3842052d8963
Marc Clifton <marc.clifton@gmail.com>
2014-12-16 22:00:02 -0500

    Work on applets and the web page viewer receptor using Chromium.

--- Receptors/Web/WebBrowserReceptor/WebBrowserReceptor.cs
+++ Receptors/Web/WebBrowserReceptor/WebBrowserReceptor.cs
@@ -16,12 +16,20 @@ using Clifton.Receptor.Interfaces;
 using Clifton.SemanticTypeSystem.Interfaces;
 using Clifton.Tools.Strings.Extensions;
 
+// Only perform layout when control has completly finished resizing
+// ResizeBegin += (s, e) => SuspendLayout();
+// ResizeEnd += (s, e) => ResumeLayout(true);
+
 namespace WebBrowserReceptor
 {
     public class WebBrowserReceptor : WindowedBaseReceptor
     {
 		// protected WebBrowser browser;
 		protected IWinFormsWebBrowser browser;
+		protected string internalUrl = "www.google.com";
+
+		[MycroParserInitialize("tbAddress")]
+		protected TextBox tbAddress;
 
 		public override string Name { get { return "Web Page Viewer"; } }
 		public override bool IsEdgeReceptor { get { return true; } }
@@ -64,29 +72,36 @@ namespace WebBrowserReceptor
 
 		protected void ShowPage(string url)
 		{
-			form.IfNull(() => ReinitializeUI());
-
+			internalUrl = url;
+			form.IfNull(() => ReinitializeUI()).Else(() => browser.Load(url));
 			Subname = WindowName;
-			
-			//if (!url.BeginsWith("http"))
-			//{
-			//	url = "http://" + url;
-			//}
-
-			// browser.Navigate(new Uri(url));
-
-			browser.Load(url);
 		}
 
-		protected override void PostFormCreate()
+		protected override void InitializeUI()
 		{
- 			base.PostFormCreate();
-
-			string url = (String.IsNullOrEmpty(UserUrl) ? "www.google.com" : UserUrl);
-			browser = new ChromiumWebBrowser(url);
-			((Control)browser).Dock = DockStyle.Fill;
-			form.Controls.Add((Control)browser);
-			// browser = (WebBrowser)mycroParser.ObjectCollection["browser"];
+			base.InitializeUI();
+			browser = (ChromiumWebBrowser)mycroParser.ObjectCollection["browser"];
+			browser.Load(internalUrl);
 		}
-    }
+
+		protected void OnAddressKey(object sender, KeyEventArgs key)
+		{
+			if (key.KeyCode == Keys.Enter)
+			{
+				key.Handled = key.SuppressKeyPress = true;		// Suppress beep
+				internalUrl = tbAddress.Text;
+				browser.Load(internalUrl);
+			}
+		}
+
+		protected void OnBrowserTitleChanged(object sender, TitleChangedEventArgs args)
+		{
+			((Control)browser).BeginInvoke(() => Subname = args.Title);
+		}
+	
+		private void OnBrowserAddressChanged(object sender, AddressChangedEventArgs args)
+		{
+			((Control)browser).BeginInvoke(() => tbAddress.Text = args.Address);
+		}
+	}
 }
